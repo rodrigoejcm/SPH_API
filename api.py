799ce1194e004a75e9655180d3fd06e62544ddc2
rodrigoejcm <rodrigo.ejcm@gmail.com>
2019-04-09 13:40:10 +0100

    change of endpoints

--- api.py
+++ api.py
@@ -23,7 +23,9 @@ load_models()
 ### LOAD PARSER
 
 parser = reqparse.RequestParser()
-parser.add_argument('query')
+parser.add_argument('content')
+parser.add_argument('news_title')
+parser.add_argument('headline')
 parser.add_argument('model')
 parser.add_argument('lang')
 
@@ -33,9 +35,16 @@ class PredictHateSpeech(Resource):
 
         # GET PARAMS
         args = parser.parse_args()
-        user_query = args['query']
+        
+        ## General
+        user_query = args['content']
         user_model = args['model']
         user_lang = args['lang']
+
+        print('[INFO REQUEST] Received Parameters')
+        print('[INFO REQUEST - content ] ', user_query)
+        print('[INFO REQUEST - Model ] ', user_model)
+        print('[INFO REQUEST - Lang ] ', user_lang)
 
         application.logger.info('[LOG] Query: ', user_query)
 
@@ -46,7 +55,42 @@ class PredictHateSpeech(Resource):
         return result_prediction
 
 
-api.add_resource(PredictHateSpeech, '/')
+api.add_resource(PredictHateSpeech, '/comments_api')
+
+
+class PredictNewsHateSpeech(Resource):
+    def get(self):
+
+        # GET PARAMS
+        args = parser.parse_args()
+        
+        ## Specifc News
+        news_headline = args['headline']
+        news_title = args['news_title']
+        
+        ## General
+        user_query = args['content']
+        user_model = args['model']
+        user_lang = args['lang']
+
+        print('[INFO REQUEST] Received Parameters')
+        print('[INFO REQUEST - content ] ', user_query)
+        print('[INFO REQUEST - headline ] ', news_headline)
+        print('[INFO REQUEST - news_title ] ', news_title)
+        print('[INFO REQUEST - Model ] ', user_model)
+        print('[INFO REQUEST - Lang ] ', user_lang)
+
+        application.logger.info('[LOG] Query: ', user_query)
+
+        # PREDICT 
+
+        result_prediction = predict(user_query,user_lang,user_model)
+
+        return result_prediction
+
+
+api.add_resource(PredictNewsHateSpeech, '/news_api')
+
 
 
 if __name__ == '__main__':
